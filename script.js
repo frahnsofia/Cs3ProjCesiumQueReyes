a88161a5e6abda941a7c43a1648ce0f7094c4c20
Glitch (glitch-hello-website) <none>
2024-11-02 03:07:33 +0000

    🍯🎃 Checkpoint ./script.js:70084687/308 ./Webpage 1.html:70084687/259 ./Webpage 2.html:70084687/11 ./place.html:70084687/6

--- script.js
+++
@@ -1,7 +0,0 @@
-    function playGame() {
-        document.getElementById("response").innerText = "Great! Click the link in Chapter 2 to continue!"
-    }
-
-    function notPlayGame() {
-        document.getElementById("response").innerText = "Maybe next time!"
-    }
