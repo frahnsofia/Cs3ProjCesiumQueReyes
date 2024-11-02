5e842d9c03a81ea925caea5edc7413a41da50d18
Glitch (glitch-hello-website) <none>
2024-11-02 02:56:32 +0000

    🖇⛵️ Checkpoint ./place.html:70084687/610 ./Webpage 1.html:70084687/270 ./script.js:70084687/292

---
+++ script.js
@@ -0,0 +1,7 @@
+    function playGame() {
+        document.getElementById("response").innerText = "Great! Click the link in Chapter 2 to continue!"
+    }
+
+    function notPlayGame() {
+        document.getElementById("response").innerText = "Maybe next time!"
+    }
