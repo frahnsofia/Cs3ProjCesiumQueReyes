81570312c95b1f9ddb37661714fdb472cac9bc38
Glitch (glitch-hello-website) <none>
2024-12-20 16:10:39 +0000

    😂👨‍❤️‍💋‍👨 Checkpoint ./script.js:70051488/1418 ./index.html:70051488/1183 ./Webpage 1.html:70051488/1024

---
+++ script.js
@@ -0,0 +1,52 @@
+var Webpage = 0
+var ad1 = document.getElementById("ad1")
+var ad2 = document.getElementById("ad2")
+var ad3 = document.getElementById("ad3")
+
+function ad(x) {
+  Webpage = x
+  let ad = Math.floor(Math.random() * 3) + 1
+  if (ad==1) {
+    document.getElementById("body").style.visibility = "hidden"
+    ad1.style.visibility = "visible"
+    ad1.volume = 0.08
+    ad1.play()
+    const skip = setTimeout(visibility, 5000)
+    const end = setTimeout(redirect, 21826)
+  }
+  else if (ad==2) {
+    document.getElementById("body").style.visibility = "hidden"
+    ad2.style.visibility = "visible"
+    ad2.volume = 0.03
+    ad2.play()
+    const skip = setTimeout(visibility, 5000)
+    const end = setTimeout(redirect, 31834)
+  }
+  else if (ad==3) {
+    document.getElementById("body").style.visibility = "hidden"
+    ad3.style.visibility = "visible"
+    ad3.volume = 0.2
+    ad3.play()
+    const skip = setTimeout(visibility, 5000)
+    const end = setTimeout(redirect, 44837)
+  }
+}
+
+function visibility() {
+  document.getElementById("x").style.visibility = "visible";
+}
+
+function redirect() {
+  if (Webpage==1) {
+    location.replace("index.html")
+  }
+  else if (Webpage==1) {
+    location.replace("Webpage1.html")
+  }
+  else if (Webpage==2) {
+    location.replace("Webpage2.html")
+  }
+  else if (Webpage==3) {
+    location.replace("Webpage3.html")
+  }
+}
